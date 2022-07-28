94641e497f223d60ba4af2a1cce13642776fa186
Dora Do <dora.thien.do@gmail.com>
2022-07-28 13:33:11 -0400

    init

---
+++ src/gestures/index.js
@@ -0,0 +1,9 @@
+import VictoryGesture from './Victory';
+import ThumbsUpGesture from './ThumbsUp';
+import PointLeftGesture from './PointLeft';
+
+export {
+  VictoryGesture,
+  ThumbsUpGesture,
+  PointLeftGesture
+}
